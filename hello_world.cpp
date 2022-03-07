0d7c23048d184765de334caf2efeeda4a641039a
Arthur-Kitsuragi <l-a-03@inbox.ru>
2022-03-08 02:39:40 +0300

    good style with notes

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@ include <iostream>
 include <string>
 
 int main(){
-	string name;
+	string name; //note1
 
-	std::cout<<"enter your name"<<std::endl;
+	std::cout<<"enter your name"<<std::endl; //note2
 	std::cin>>name;
 	std::cout<<"Hello world from "<<name;
 
